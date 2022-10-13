5cd7c582125063ce624f0557f65161e0b2d66725
Jason Walton <jwalton@solinkcorp.com>
2022-10-13 00:11:53 -0400

    fix: Fix bumping v1.

--- bin/retagMajorVersion.sh
+++ bin/retagMajorVersion.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+VERSION=$(node -e "console.log('v' + require('./package.json').version)")
 MAJOR_VERSION=$(node -e "console.log('v' + require('./package.json').version.split('.')[0])")
 
-echo "Retagging ${MAJOR_VERSION}"
+echo "Retagging ${MAJOR_VERSION} to ${VERSION}"
 git tag -d ${MAJOR_VERSION} && \
   git push --delete origin ${MAJOR_VERSION} && \
-  git tag -a ${MAJOR_VERSION} -m ${NEXT_VERSION} && \
+  git tag -a ${MAJOR_VERSION} -m ${VERSION} && \
   git push --follow-tags
