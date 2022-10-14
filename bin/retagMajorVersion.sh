59677d99be71436c0f395e22c1f4308f7e69f398
Jason Walton <jwalton@solinkcorp.com>
2022-10-14 09:49:13 -0400

    ci: Do not delete existing major tag if it doesn't exist. [skip ci]

--- bin/retagMajorVersion.sh
+++ bin/retagMajorVersion.sh
@@ -5,8 +5,10 @@ set -e
 VERSION=$(node -e "console.log('v' + require('./package.json').version)")
 MAJOR_VERSION=$(node -e "console.log('v' + require('./package.json').version.split('.')[0])")
 
+if ! git rev-parse "$1" >/dev/null 2>&1; then
+  echo "Deleting ${MAJOR_VERSION}"
+  git tag -d ${MAJOR_VERSION} && git push --delete origin ${MAJOR_VERSION}
+fi
+
 echo "Retagging ${MAJOR_VERSION} to ${VERSION}"
-git tag -d ${MAJOR_VERSION} && \
-  git push --delete origin ${MAJOR_VERSION} && \
-  git tag -a ${MAJOR_VERSION} -m ${VERSION} && \
-  git push --follow-tags
+git tag -a ${MAJOR_VERSION} -m ${VERSION} && git push --follow-tags
